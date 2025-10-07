1aa70c75e8e948b26058d7e98c08c8c89ebae12a
dipasmitak <dipasmita.kashyap@gmail.com>
2025-10-07 17:17:30 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+text = "Hello, Dipasmita!"
+
+print(text)
+
+print("Length:", len(text))
+
+print("Uppercase:", text.upper())
+
+print("Lowercase:", text.lower())
+
+new_text = text.replace("Dipasmita", "Friend")
+print("Replace:", new_text)
+
+print("First 5 letters:", text[0:5])
+
+print("Is 'Hello' in text?", "Hello" in text)
+
+print("Reversed text:", text[::-1])
